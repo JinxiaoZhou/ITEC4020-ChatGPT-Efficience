823b7e43e25bed2929eeddd827f5de064d73beb1
zijingw <109635814+SamanthaWang0830@users.noreply.github.com>
2024-11-13 23:22:12 -0500

    change again

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import {History, SocialScience, ComputerSecurity} from '../schema.js'
+import {History, SocialScience, ComputerSecurity} from './schema.js'
 import 'dotenv/config'
 import OpenAI from "openai";
 
